52f4deb478dd5181e43572af3a558200c76ef82f
Ignadius <ignaa91@gmail.com>
2026-06-13 22:16:14 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="me.png" alt="Ignacio B" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Ignacio B</h1>
+      <p>
+        Junior web developer and fitness lover. When not coding or preparing a
+        workout, I like to play video games, to cook (and eat), or to just enjoy
+        the Tenerife's sun.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
