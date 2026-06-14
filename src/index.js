c3245420353c833aa902fcefbd382d736e3aac0a
Ignadius <ignaa91@gmail.com>
2026-06-15 01:07:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+// 1. Extract data into an array of objects
+const skillsData = [
+  { skill: "React", emoji: "💪", color: "blue" },
+  { skill: "HTML+CSS", emoji: "💪", color: "orange" },
+  { skill: "JavaScript", emoji: "💪", color: "yellow" },
+  // Easy to add more skills here!
+];
+
 function App() {
   return (
     <div className="card">
       <Avatar />
       <div className="data">
         <Intro />
-
-        <SkillList />
+        {/* Pass the data down as a prop */}
+        <SkillList skills={skillsData} /> 
       </div>
     </div>
   );
 }
 
 function Avatar() {
+  // Use the imported image here if you set that up: src={avatarImg}
   return <img className="avatar" src="me.png" alt="Ignacio B" />;
 }
 
@@ -32,21 +41,28 @@ function Intro() {
   );
 }
 
-function SkillList() {
+// 2. Accept the skills array and map over it
+function SkillList({ skills }) {
   return (
     <div className="skill-list">
-      <Skill skill="React" emoji="💪" color="blue" />
-      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
-      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      {skills.map((item) => (
+        <Skill 
+          key={item.skill} // Unique key required when mapping
+          skill={item.skill} 
+          emoji={item.emoji} 
+          color={item.color} 
+        />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+// 3. Destructured props
+function Skill({ skill, emoji, color }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>{emoji}</span>
     </div>
   );
 }
